0e0356638d06364c6d849fba8aa814823f0529a3
krchkrch <pravdivtsev.ilya@mail.ru>
2018-09-16 23:33:13 +0300

    hw01

--- hw01/hw01_2.java
+++ hw01/hw01_2.java
@@ -1,19 +1,24 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 
-public class hw01_1 {
+public class hw01_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("N:");
         int n = sc.nextInt();
         int a = 0;
+        int b = 0;
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
-            if (Math.abs(x) > a) {
-                a = Math.abs(x);
-            }
-
-
+            if (x > a) {
+                b = a;
+                a = x;
+            } else if (x > b)
+                b = x;
         }
+
+
+
         System.out.println(a);
-    }
+        System.out.println(b);
+}
 }
